2f25d3e86e2090a3292bf328778233ca4f5b7853
AgentTroll <woodyc40@gmail.com>
2013-12-07 17:10:37 -0800

    Update Players.java

--- src/main/java/com/gmail/woodyc40/tenjava/Players.java
+++ src/main/java/com/gmail/woodyc40/tenjava/Players.java
@@ -9,9 +9,10 @@ import org.bukkit.entity.Player;
  */
 public class Players {
 
-    public String name;
-    int mobSpawn;
-    int wins;
+    public String name = "";
+    int mobSpawn = 0;
+    int wins = 0;
+    int kills = 0;
 
     public Players(Player p, int num, int i) {
         name = p.getName();
@@ -23,16 +24,18 @@ public class Players {
         return mobSpawn;
     }
 
-    public void increment() {
+    public void increment(int kills) {
         wins++;
         if(wins % 4 == 0) {
             mobSpawn++;
         }
+        this.kills =+ kills;
     }
 
     public String getScore() {
         return ChatColor.GOLD + "Your Mob Spawn Rate: " + ChatColor.DARK_BLUE + mobSpawn + "\n" 
-            + ChatColor.GOLD + "Your wins: " + ChatColor.DARK_BLUE + wins;
+            + ChatColor.GOLD + "Your wins: " + ChatColor.DARK_BLUE + wins + "\n"
+            + ChatColor.GOLD + "Your total kills: " + ChatColor.DARK_BLUE + kills;
     }
 
 }
