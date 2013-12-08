af0285d04fcfe678608c7cde691713b6491b5059
AgentTroll <woodyc40@gmail.com>
2013-12-07 16:23:36 -0800

    Update Computer.java

--- src/main/java/com/gmail/woodyc40/tenjava/Computer.java
+++ src/main/java/com/gmail/woodyc40/tenjava/Computer.java
@@ -8,6 +8,18 @@ public class Computer {
 
     public List<UUID> uuids = new ArrayList<>();
 
-    //schedule spawns and place tjere
+    Game game = null;
+    public Computer(Game g) {
+        game = g;
+    }
+
+    public void spawn(int in, EntityType et) {
+        if(new Random().nextBoolean()) {
+            for(int x = 0; x <= in * new Random().nextInt(6); x++) {
+                Entity ent = game.getWorld().spawnEntity(game.getSpawn(), et);
+                uuids.add(ent.getUniqueId()); //TODO Set target at spawn, spawn away from the spawn
+            }
+        }
+    }
 
 }
