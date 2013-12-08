53bc7367275c3748b121800f4d932d1ffd494eeb
AgentTroll <woodyc40@gmail.com>
2013-12-07 16:46:03 -0800

    Update Score.java

--- src/main/java/com/gmail/woodyc40/tenjava/command/subcommands/Score.java
+++ src/main/java/com/gmail/woodyc40/tenjava/command/subcommands/Score.java
@@ -1,7 +1,23 @@
+/**
+ * Name: Score.java
+ * Created: 7 December 2013
+ *
+ * @version 1.0
+ * @author AgentTroll
+ */
 package com.gmail.woodyc40.tenjava.command.subcommands;
 
-import
+import com.gmail.woodyc40.tenjava.command.SubCommand;
+import com.gmail.woodyc40.tenjava.managers.GameManager;
+import com.gmail.woodyc40.tenjava.managers.MessageManager;
+import org.bukkit.entity.Player;
 
 public class Score implements SubCommand {
+
+    public void execute(Player p, String cmd, String[] args) {
+        if(cmd.equalsIgnoreCase("stats")) {
+            p.sendMessage(MessageManager.getInstance().getPrefix() + );
+        }
+    }
 
 }
